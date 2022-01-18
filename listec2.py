82ac760403c10cf60a7191cdc4b651e8a85391e1
mohankrish0207 <86762895+mohankrish0207@users.noreply.github.com>
2022-01-18 08:55:31 +0530

    python file

---
+++ listec2.py
@@ -0,0 +1,15 @@
+import boto3
+import sys
+
+accesskey= sys.argv[1]
+secretkey= sys.argv[2]
+region= sys.argv[3]
+client = boto3.client('ec2',region_name = region, aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
+
+myec2 = client.describe_instances()
+for printins in myec2['Reservations']:
+	for printout in printins['Instances']:
+		print(printout['InstanceId'])
+		print(printout['InstanceType'])
+		print(printout['LaunchTime'])
+		print(printout['State']['Name'])
